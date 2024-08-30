883a723d9cab1d598e1094f8517e33ab6e55374e
Docta Rhyf <drrhyf@gmail.com>
2024-08-30 15:55:55 +0200

    working on axios

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  async function loadData() {
+    const d = await axios.get("https://www.yahoo.com/");
+    console.log(d);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>this is cool!</Text>
+      <Text>his is axio {JSON.stringify(axios.get)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
