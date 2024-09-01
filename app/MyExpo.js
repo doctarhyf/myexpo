e9419a9370fe062fa9ff71f4acd846d9985f41d5
Docta Rhyf <drrhyf@gmail.com>
2024-09-01 21:48:07 +0200

    routing

--- app/MyExpo.js
+++ app/MyExpo.js
@@ -31,10 +31,17 @@ export default function MyExpo() {
     const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
     const color = item.id === selectedId ? "white" : "black";
 
+    const handlePress = () => {
+      router.push({
+        pathname: "/item",
+        params: { ...item },
+      });
+    };
+
     return (
       <Item
         item={item}
-        onPress={() => setSelectedId(item.id)}
+        onPress={handlePress}
         backgroundColor={backgroundColor}
         textColor={color}
       />
--- app/item.js
+++ app/item.js
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
-export default function Item() {
+export default function Item({}) {
+  const item = useLocalSearchParams();
+
   return (
     <View>
-      <Text>Item</Text>
+      <Text>Item {JSON.stringify(item)}</Text>
+      <Pressable onPress={(e) => router.back()}>
+        <Text>Back</Text>
+      </Pressable>
     </View>
   );
 }
